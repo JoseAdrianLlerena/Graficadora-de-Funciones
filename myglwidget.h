ed454895d626e1a995baccd6eabe38b403c6bb7b
diegoamusicr <diegoamusicr@gmail.com>
2016-05-14 14:44:18 -0500

    Create myglwidget.h

---
+++ myglwidget.h
@@ -0,0 +1,46 @@
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QGLWidget>
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MyGLWidget(QWidget *parent = 0);
+    ~MyGLWidget();
+signals:
+
+public slots:
+
+protected:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+public slots:
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+
+signals:
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
+
+private:
+    void draw();
+
+    int xRot;
+    int yRot;
+    int zRot;
+
+    QPoint lastPos;
+};
+
+#endif // MYGLWIDGET_H
